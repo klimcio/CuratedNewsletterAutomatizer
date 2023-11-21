7757289ba903dd31212e6393b119250781926d2a
Mariusz Klimek <klimcio@gmail.com>
2023-11-21 23:04:49 +0100

    Keep configuration in a separate file

---
+++ GodotWeeklyAutomations/NewIssueService/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace NewIssueService;
+
+internal class Configuration
+{
+    public string AirtableApiAccessToken { get; set; } = default!;
+    public string AirtableApiGetTableContentEndpoint { get; set; } = default!;
+}
--- GodotWeeklyAutomations/NewIssueService/Program.cs
+++ GodotWeeklyAutomations/NewIssueService/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace NewIssueService;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace NewIssueService;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json")
+            .Build()
+            .GetRequiredSection("Settings")
+            .Get<Configuration>();
+
+
+
         Console.WriteLine("Hello, World!");
     }
 }
