a04fb0fb1dcc9c2e06628f988c698c3eb62a62e9
Mariusz Klimek <klimcio@gmail.com>
2023-11-23 08:50:59 +0100

    Getting records from AirTable

---
+++ GodotWeeklyAutomations/NewIssueService/AirTableRecords.cs
@@ -0,0 +1,28 @@
+﻿namespace NewIssueService;
+
+internal class AirTableRecords
+{
+    public IEnumerable<AirTableRecord> Records { get; set; } = new List<AirTableRecord>();
+}
+
+internal class AirTableRecord
+{
+    public string Id { get; set; } = default!;
+    public string CreatedTime { get; set; }
+    public Dictionary<string, string> Fields { get; set; } = new Dictionary<string, string>();
+}
+
+internal static class AirTableRecordExtensions
+{
+    public static string Name(this AirTableRecord record) => record.Fields.GetKeyOrEmptyString("Name");
+    public static string Status(this AirTableRecord record) => record.Fields.GetKeyOrEmptyString("Status");
+    public static string Url(this AirTableRecord record) => record.Fields.GetKeyOrEmptyString("URL");
+    public static string Source(this AirTableRecord record) => record.Fields.GetKeyOrEmptyString("Source");
+    public static string Notes(this AirTableRecord record) => record.Fields.GetKeyOrEmptyString("Notes");
+    public static string TargetCategory(this AirTableRecord record) => record.Fields.GetKeyOrEmptyString("TargetCategory");
+    public static string NewUrl(this AirTableRecord record) => record.Fields.GetKeyOrEmptyString("NewURL");
+    public static string NewDescription(this AirTableRecord record) => record.Fields.GetKeyOrEmptyString("NewDescription");
+
+    private static string GetKeyOrEmptyString(this Dictionary<string, string> dict, string key) 
+        => dict.ContainsKey(key) ? dict[key] : string.Empty;
+}
--- GodotWeeklyAutomations/NewIssueService/Program.cs
+++ GodotWeeklyAutomations/NewIssueService/Program.cs
@@ -1,19 +1,54 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace NewIssueService;
 
 internal class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        var configuration = new ConfigurationBuilder()
+        var configuration = GetConfiguration();
+
+        if (configuration is null || 
+            string.IsNullOrWhiteSpace(configuration?.AirtableApiGetTableContentEndpoint) || 
+            string.IsNullOrWhiteSpace(configuration?.AirtableApiAccessToken))
+        {
+            Console.WriteLine("Configuration not set");
+            return;
+        }
+
+        var records = (await configuration.AirtableApiGetTableContentEndpoint.GetContentAsync(
+            configuration.AirtableApiAccessToken
+        )).Parse();
+
+        var nextIssueRecords = records?.Records.Where(x => AirTableRecordExtensions.Status(x) == "Next issue");
+
+        Console.WriteLine("Hello, World!");
+    }
+
+    private static Configuration? GetConfiguration() =>
+        new ConfigurationBuilder()
             .AddJsonFile($"appsettings.json")
             .Build()
             .GetRequiredSection("Settings")
             .Get<Configuration>();
+}
 
+internal static class Extensions
+{ 
+    public static async Task<string> GetContentAsync(this string airTableEndpoint, string airTableToken)
+    {
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", airTableToken);
+        var rawRecords = await client.GetStringAsync(airTableEndpoint);
 
-
-        Console.WriteLine("Hello, World!");
+        return rawRecords;
     }
+
+    public static AirTableRecords? Parse(this string rawRecords) 
+        => JsonSerializer.Deserialize<AirTableRecords>(rawRecords, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
 }
